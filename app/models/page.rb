18895a328d7f2ff6cd73b1f646650c0accf48935
Taufiq Muhammadi <waruboy@yahoo.com>
2016-08-27 19:41:32 +0700

    add url format validation in Page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,6 +2,6 @@ class Page < ActiveRecord::Base
   has_many :links, dependent: :destroy
   has_many :headers, dependent: :destroy
 
-  validates :url, presence: true
+  validates :url, presence: true, format: URI.regexp
   validates :status, presence: true
 end
